7d912d74344af0512ac9a9d899d0358cdb9dc64d
Tina Kovalova <kovalevatkv@gmail.com>
2023-03-16 22:59:37 +0200

    swiper breakpoints

--- js/index.js
+++ js/index.js
@@ -9,12 +9,22 @@ const sliderItems= slider.children;
 
 // SLIDER*******************
 const swiper = new Swiper(".swiper", {
-  slidesPerView: 2,
-  spaceBetween:30,
+ 
+  spaceBetween:10,
   loop:true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
+    0: {
+      slidesPerView: 1,
+      direction:'horizontal',
+    },
+    1500: {
+      slidesPerView: 2,
+      direction:'horizontal'
+    }
   }
 });
 // SLIDER*******************
